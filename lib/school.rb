e6db3ddc0eadd4b72309485a5467f19a05bd4239
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-03-14 08:35:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+  
+  def grade(grade)
+    roster.each do |key, value|
+      if key == grade
+        value
+      end
+    end
+  end 
+  
+  def alphabatically
+    @roster.sort_by do |key, value|
+      key
+    end
+  end
+end
